d6c5f83b26d10e9420510d3bf7c429c83c23f6dc
abiz <ab.zare@emofid.com>
2025-08-31 16:59:27 +0330

    add log in files

--- src/main/kotlin/proxy/ProxyServer.kt
+++ src/main/kotlin/proxy/ProxyServer.kt
@@ -1,5 +1,6 @@
 package proxy
 
+import java.io.File
 import java.io.FileInputStream
 import java.net.InetSocketAddress
 import java.nio.ByteBuffer
@@ -7,22 +8,43 @@ import java.nio.channels.SelectionKey
 import java.nio.channels.Selector
 import java.nio.channels.ServerSocketChannel
 import java.nio.channels.SocketChannel
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 fun main() {
     val properties = Properties().apply { load(FileInputStream("config.properties")) }
     val listeningPort = properties.getProperty("server_port").trim().toInt()
     val proxyPort = properties.getProperty("proxy_port").trim().toInt()
+    val requestLog = properties.getProperty("request_log").trim()
+    val responseLog = properties.getProperty("response_log").trim()
     println("Starting proxy port:$listeningPort to $proxyPort")
-    ProxyServer(listeningPort, proxyPort).start()
+    ProxyServer(listeningPort, proxyPort, File(requestLog), File(responseLog)).start()
 }
 
-class ProxyServer(private val listeningPort: Int, private val backendPort: Int) {
+enum class FlowSide {
+    FROM_CLIENT, TO_SERVER;
+
+    fun flip() = when (this) {
+        FROM_CLIENT -> TO_SERVER
+        TO_SERVER -> FROM_CLIENT
+    }
+}
+
+class ProxyServer(
+    private val listeningPort: Int,
+    private val backendPort: Int,
+    private val requestLog: File,
+    private val responseLog: File
+) {
     var count = 0
 
     private data class Session(
         val buffer: ByteBuffer,
-        val peerChannel: SocketChannel
+        val peerChannel: SocketChannel,
+        val flowSide: FlowSide
     )
 
     fun start() {
@@ -50,12 +72,12 @@ class ProxyServer(private val listeningPort: Int, private val backendPort: Int) 
                         val buffer = ByteBuffer.allocate(1024)
                         balancerChannel.register(
                             selector, SelectionKey.OP_READ,
-                            Session(buffer, client)
+                            Session(buffer, client, FlowSide.TO_SERVER)
                         )
                         client.configureBlocking(false)
                         client.register(
                             selector, SelectionKey.OP_CONNECT,
-                            Session(buffer, balancerChannel)
+                            Session(buffer, balancerChannel, FlowSide.FROM_CLIENT)
                         )
                         count++
                         if (count % 1000 == 0)
@@ -77,7 +99,7 @@ class ProxyServer(private val listeningPort: Int, private val backendPort: Int) 
                         } else
                             session.peerChannel.register(
                                 selector, SelectionKey.OP_WRITE,
-                                Session(session.buffer, key.channel() as SocketChannel)
+                                Session(session.buffer, key.channel() as SocketChannel, session.flowSide.flip())
                             )
                     }
 
@@ -99,8 +121,26 @@ class ProxyServer(private val listeningPort: Int, private val backendPort: Int) 
         val buffer = (key.attachment() as Session).buffer
         kotlin.runCatching {
             (key.channel() as SocketChannel).write(buffer)
+            logContent((key.attachment() as Session).flowSide, buffer)
         }
         buffer.flip()
+    }
+
+    private fun logContent(flowSide: FlowSide, buffer: ByteBuffer) {
+        val now = Instant.now()
+        val localDateTime = ZonedDateTime.ofInstant(now, ZoneId.systemDefault())
+
+        val formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS")
+        val formatted = localDateTime.format(formatter)
+        val socketContent = "\n--------------------$formatted----------------------\n" +
+                (buffer.arrayOffset() until buffer.limit())
+                    .map { buffer[it].toInt().toChar() }
+                    .joinToString("")
+
+        when (flowSide) {
+            FlowSide.FROM_CLIENT -> requestLog.appendText(socketContent)
+            FlowSide.TO_SERVER -> responseLog.appendText(socketContent)
+        }
     }
 
     private fun handleRead(key: SelectionKey): Int {
